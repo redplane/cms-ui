3cfc63690d1e78b10db2e7a0db148cf798b244c3
linh.nguyen <lightalakanzam@gmail.com>
2021-04-24 15:37:51 +0700

    - Fixed unit test - Only deploy to npmjs when code is merged to master - Only deploy to myget when code is merged to dev

--- projects/cms-ui/src/modules/validator/validation-summarizer/validation-summarizer.component.spec.ts
+++ projects/cms-ui/src/modules/validator/validation-summarizer/validation-summarizer.component.spec.ts
@@ -1,6 +1,6 @@
 import {TestBed} from '@angular/core/testing';
-import {ValidationSummarizerComponent} from '@cms-ui/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ValidationSummarizerComponent} from './validation-summarizer.component';
 import {Subscription} from 'rxjs';
 
 describe('ValidationSummarizerComponent', () => {
